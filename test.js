acebc63d76596af23b7f4132e57eb2ee0e9ab841
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-03-17 09:05:25 +0100

    Harden against common degenerate cases (#4)

--- test.js
+++ test.js
@@ -232,6 +232,193 @@ Second chunk\r
   )
 })
 
+test('request, conflicting content-length and transfer-encoding', async (t) => {
+  const parser = new HTTPParser()
+
+  const input = `POST /users HTTP/1.1\r
+Host: example.com\r
+Content-Length: 10\r
+Transfer-Encoding: chunked\r
+\r
+5\r
+hello\r
+0\r
+\r\n`
+
+  await t.exception(() => [...parser.push(input)], /INVALID/)
+})
+
+test('request, transfer-encoding chunked case insensitive', (t) => {
+  const parser = new HTTPParser()
+
+  const input = `POST /users HTTP/1.1\r
+Host: example.com\r
+Transfer-Encoding: Chunked\r
+\r
+5\r
+hello\r
+0\r
+\r\n`
+
+  const results = [...parser.push(input)]
+
+  t.is(results[0].type, REQUEST)
+  t.is(results[1].type, DATA)
+  t.alike(results[1].data, Buffer.from('hello'))
+  t.is(results[2].type, END)
+})
+
+test('request, header exceeds max size', async (t) => {
+  const parser = new HTTPParser({ maxHeaderSize: 64 })
+
+  const input = `GET /users HTTP/1.1\r
+Host: example.com\r
+X-Large: ${'A'.repeat(100)}\r
+\r
+`
+
+  await t.exception(() => [...parser.push(input)], /INVALID/)
+})
+
+test('request, too many headers', async (t) => {
+  const parser = new HTTPParser({ maxHeadersCount: 3 })
+
+  const headers = Array.from({ length: 5 }, (_, i) => `X-Header-${i}: value\r`).join('\n')
+
+  const input = `GET /users HTTP/1.1\r
+Host: example.com\r
+${headers}
+\r
+`
+
+  await t.exception(() => [...parser.push(input)], /INVALID/)
+})
+
+test('request, duplicate content-length', async (t) => {
+  const parser = new HTTPParser()
+
+  const input = `POST /users HTTP/1.1\r
+Host: example.com\r
+Content-Length: 5\r
+Content-Length: 10\r
+\r
+hello`
+
+  await t.exception(() => [...parser.push(input)], /INVALID/)
+})
+
+test('request, duplicate transfer-encoding', async (t) => {
+  const parser = new HTTPParser()
+
+  const input = `POST /users HTTP/1.1\r
+Host: example.com\r
+Transfer-Encoding: chunked\r
+Transfer-Encoding: chunked\r
+\r
+5\r
+hello\r
+0\r
+\r\n`
+
+  await t.exception(() => [...parser.push(input)], /INVALID/)
+})
+
+test('request, invalid header name', async (t) => {
+  const parser = new HTTPParser()
+
+  const input = `GET /users HTTP/1.1\r
+Host: example.com\r
+Invalid Header: value\r
+\r
+`
+
+  await t.exception(() => [...parser.push(input)], /INVALID_HEADER/)
+})
+
+test('request, null byte in header name', async (t) => {
+  const parser = new HTTPParser()
+
+  const input = Buffer.concat([
+    Buffer.from('GET /users HTTP/1.1\r\nHost: example.com\r\nX-Bad'),
+    Buffer.from([0x00]),
+    Buffer.from(': value\r\n\r\n')
+  ])
+
+  await t.exception(() => [...parser.push(input)], /INVALID_HEADER/)
+})
+
+test('request, header without space after colon', (t) => {
+  const parser = new HTTPParser()
+
+  const input = `GET /users HTTP/1.1\r
+Host:example.com\r
+Content-Length: 0\r
+\r
+`
+
+  const results = [...parser.push(input)]
+
+  t.is(results[0].type, REQUEST)
+  t.is(results[0].headers.host, 'example.com')
+})
+
+test('request, content-length with trailing garbage', async (t) => {
+  const parser = new HTTPParser()
+
+  const input = `POST /users HTTP/1.1\r
+Host: example.com\r
+Content-Length: 5abc\r
+\r
+hello`
+
+  await t.exception(() => [...parser.push(input)], /INVALID_CONTENT_LENGTH/)
+})
+
+test('request, chunk length with trailing garbage', async (t) => {
+  const parser = new HTTPParser()
+
+  const input = `POST /users HTTP/1.1\r
+Host: example.com\r
+Transfer-Encoding: chunked\r
+\r
+5xyz\r
+hello\r
+0\r
+\r\n`
+
+  await t.exception(() => [...parser.push(input)], /INVALID_CHUNK_LENGTH/)
+})
+
+test('request, terminator split across pushes', (t) => {
+  const head = 'GET / HTTP/1.0\r\nX-Foo: bar\r\n'
+  const terminator = '\r\n'
+
+  for (let i = 1; i < terminator.length; i++) {
+    const parser = new HTTPParser()
+
+    const results = [
+      ...parser.push(Buffer.from(head + terminator.slice(0, i))),
+      ...parser.push(Buffer.from(terminator.slice(i)))
+    ]
+
+    t.is(results[0].type, REQUEST)
+    t.is(results[0].headers['x-foo'], 'bar')
+  }
+})
+
+test('request, partial terminator match resets across pushes', (t) => {
+  const parser = new HTTPParser()
+
+  const results = [
+    ...parser.push(Buffer.from('GET / HTTP/1.0\r\n')),
+    ...parser.push(Buffer.from('X-Foo: bar\r\n')),
+    ...parser.push(Buffer.from('\r\n'))
+  ]
+
+  t.is(results[0].type, REQUEST)
+  t.is(results[0].headers['x-foo'], 'bar')
+})
+
 test('request, byte by byte', (t) => {
   const parser = new HTTPParser()
 
